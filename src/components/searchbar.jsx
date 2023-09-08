79357d6a3feb72431063de5a0483faf1364e8568
danny-817 <d_halsall@hotmail.com>
2023-09-08 12:31:22 +0100

    task 10 in progress, adding sortby functionality

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -3,8 +3,12 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { getTopics } from "../utilities/api";
 
-const SearchBar = () => {
+const SearchBar = ({ filterBy, setFilterBy }) => {
   const [topics, setTopics] = useState([]);
+
+  const changeHandler = (e) => {
+    setFilterBy(e.target.value);
+  };
 
   useEffect(() => {
     getTopics(setTopics);
@@ -31,6 +35,46 @@ const SearchBar = () => {
             );
           })}
         </ul>
+        <section>
+          <h2>Sort by:</h2>
+          <form action="">
+            <div>
+              <input
+                value="Date"
+                id="radio1"
+                type="radio"
+                name="sortby"
+                checked={filterBy === "Date"}
+                onChange={changeHandler}
+              />
+              <label htmlFor="radio1">Date</label>
+            </div>
+            <br />
+            <div>
+              <input
+                value="Comment Count"
+                id="radio2"
+                type="radio"
+                name="sortby"
+                checked={filterBy === "Comment Count"}
+                onChange={changeHandler}
+              />
+              <label htmlFor="radio2">Comment Count</label>
+            </div>
+            <br />
+            <div>
+              <input
+                value="Votes"
+                id="radio3"
+                type="radio"
+                name="sortby"
+                checked={filterBy === "Votes"}
+                onChange={changeHandler}
+              />
+              <label htmlFor="radio3">Votes</label>
+            </div>
+          </form>
+        </section>
       </section>
     </>
   );
--- src/pages/allArticlesPage.jsx
+++ src/pages/allArticlesPage.jsx
@@ -2,15 +2,17 @@ import UserButton from "../components/userbutton";
 import Header from "../components/header";
 import SearchBar from "../components/searchbar";
 import Body from "../components/body";
+import { useState } from "react";
 
 const AllArticles = () => {
+  const [filterBy, setFilterBy] = useState("Date");
   return (
     <>
       <UserButton />
       <Header />
       <main>
-        <SearchBar />
-        <Body />
+        <SearchBar setFilterBy={setFilterBy} filterBy={filterBy} />
+        <Body filterBy={filterBy} />
       </main>
     </>
   );
